3d006304cccccb618c61b5cff5e04a7296ac0fc7
newbiecse <newbiecse@gmail.com>
2013-08-23 22:20:04 +0700

    final
    correct working

---
+++ DatePickerMvc4/Controllers/MyClassController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatePickerMvc4.Models;
+
+namespace DatePickerMvc4.Controllers
+{
+    public class MyClassController : Controller
+    {
+        //
+        // GET: /MyClass/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            MyClass m = new MyClass
+            {
+                MyDate = new DateTime(2013, 8, 19)
+            };
+            return View(m);
+        }
+
+    }
+}
---
+++ DatePickerMvc4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatePickerMvc4.MvcApplication" Language="C#" %>
